aa96893e897649e9a54d63b9b9e62aac480e2fe3
suman2020 <magarsuman2020@gmail.com>
2020-07-26 01:08:38 -0500

    Create Patch File

--- client3.py
+++ client3.py
@@ -35,25 +35,32 @@ def getDataPoint(quote):
 	stock = quote['stock']
 	bid_price = float(quote['top_bid']['price'])
 	ask_price = float(quote['top_ask']['price'])
-	price = bid_price
+	price = (ask_price+bid_price)/2
 	return stock, bid_price, ask_price, price
 
 def getRatio(price_a, price_b):
 	""" Get ratio of price_a and price_b """
 	""" ------------- Update this function ------------- """
 	""" Also create some unit tests for this function in client_test.py """
-	return 1
+	try:
+		ratio = price_a/price_b
+	except ZeroDivisionError:
+		print('A zero divison error has been occurred i.e price_b==0')
+		ratio = float('Inf')
+	return ratio
 
 # Main
 if __name__ == "__main__":
 
 	# Query the price once every N seconds.
-	for _ in iter(range(N)):
+	for _ in range(N):
 		quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
 		""" ----------- Update to get the ratio --------------- """
+		prices = {}
 		for quote in quotes:
 			stock, bid_price, ask_price, price = getDataPoint(quote)
-			print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+			prices[stock] = price
+			print (f"Quoted {stock} at (bid:{bid_price}, ask:{ask_price}, price:{price})")
 
-		print ("Ratio %s" % getRatio(price, price))
+		print ("Ratio %s" % (getRatio(prices['ABC'], prices['DEF'])))
